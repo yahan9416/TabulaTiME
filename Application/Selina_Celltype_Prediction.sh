318c2a2a54a150031b49cfe7ae4282edf1670b22
Deep learning by torch <44338800+yahan9416@users.noreply.github.com>
2024-08-05 14:41:53 +0800

    Delete Application directory

--- Application/Selina_Celltype_Prediction.sh
+++
@@ -1,4 +0,0 @@
-selina train --path-in /fs/home/hanya/Project/TME_Immune_difference/Selina_predict/Reference/Pretrain_result/Before_CCA_selected_DEG --path-out /fs/home/hanya/Project/TME_Immune_difference/Selina_predict/Reference/Pretrain_result/Before_CCA_selected_DEG  --outprefix Merger_beforeCCA_selectgene --disease
-
-
-selina predict --query-expr ./res/query_expr.txt --model ./res/pre-trained_params.pt --seurat ./res/query_res.rds --path-out ./res/predict/disease --disease
