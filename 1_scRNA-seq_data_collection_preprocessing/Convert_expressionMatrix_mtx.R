405c4cc7fabcdf59a147ef70ee4d67ae1c93c7ac
Deep learning by torch <44338800+yahan9416@users.noreply.github.com>
2024-09-30 23:33:48 +0800

    Delete 1_scRNA-seq_data_collection_preprocessing directory

--- 1_scRNA-seq_data_collection_preprocessing/Convert_expressionMatrix_mtx.R
+++
@@ -1,8 +0,0 @@
-Minicluster_TPM <- as(Minicluster_TPM, "sparseMatrix")
-dir="/fs/home/hanya/Project/TME_Immune_difference/TISCH_data_10X_2105/Integrated_analysis/Meger_minicluster_expression"
-mtx_file = file.path(dir, paste0("Minicluster_Merge", "_count_matrix.mtx"))
-gene_file = file.path(dir, paste0("Minicluster_Merge", "_count_genes.tsv"))
-barcode_file = file.path(dir, paste0("Minicluster_Merge", "_count_barcodes.tsv"))
-writeMM(Minicluster_TPM, mtx_file)
-write.table(rownames(Minicluster_TPM), gene_file, col.names = FALSE, row.names = FALSE, quote = FALSE)
-write.table(colnames(Minicluster_TPM), barcode_file, col.names = FALSE, row.names = F, quote = FALSE)
