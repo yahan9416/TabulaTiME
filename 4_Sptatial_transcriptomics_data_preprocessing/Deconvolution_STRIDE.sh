a4b4ce142bfc589ea8ba5d322988ffe13b84de60
Deep learning by torch <44338800+yahan9416@users.noreply.github.com>
2024-10-01 09:16:43 +0800

    Delete 4_Sptatial_transcriptomics_data_preprocessing directory

--- 4_Sptatial_transcriptomics_data_preprocessing/Deconvolution_STRIDE.sh
+++
@@ -1,5 +0,0 @@
-STRIDE deconvolve --sc-count scRNA_count_matrix.txt \
-                  --sc-celltype scRNA_meta_info.txt \
-                  --st-count  filtered_feature_bc_matrix.h5 \
-                  --outdir Result/STRIDE --outprefix Sample --normalize
-
